d0e6dbc1b896c362d8ccf6f8dc22ebc33c4f25c7
jesserwight <jessewright01@gmail.com>
2020-12-10 21:30:56 -0500

    Tidy up repository folders, remove junk

--- templates/instruction-page.js
+++ templates/instruction-page.js
@@ -4,6 +4,72 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+/*
+
+What would a very simple way to encapsulate a 'dom component' like a modal?
+
+- Create the dom node (s)
+- Provide a slot (or a few) where 'children' can exist
+- Provide some css
+
+In the end, what does it produce? What is the type of the thing that it produces?
+A dom node.
+
+- It's ok to control things with event listeners
+- BUT, can functions be passed?
+- Does the event listener need to be detached when the element is 'destroyed'
+- Does element 'destruction' ever happen?
+
+What are my other options? Just duplicate code?
+Or make stylesheets that duplicate? Colocation seems important?
+
+Flutter IS NOT ready for web. Not an option. For app? Yes, that's an option...
+But also more work?
+
+The IDEA:
+Flutter for create, web for view. You have to get the app to upload.
+
+Nobody uploads VIA computer anyways AND using a website to upload images is strange.
+// Yeah?
+
+And it provides some insentive to download the app, just by using the website.
+
+... what else? It's still the open web, with SEO and all
+
+But no janky js, maybe *no js at all*. JUST a viewer. That'd make things SO SO easy.
+
+No more modals even.
+
+What part of the world is changing the fastest? Software. And I think I want to be a part of that.
+
+So what now?
+
+I could create the view only. Stop all the CRUD stuff, and make a very nice html-tailwind rendering of what I have in figma.
+
+It also makes people 'users' of the software BEFORE they would need to download the app.
+
+Expedient or meaningful?
+
+What if this thing was just my blog?
+
+I make the frikin CMS however I want.
+
+Maybe I could do what I'm confident about: the layout
+
+Completely static. What would that hurt, starting with the end?
+
+What other problems does that seem to solve?
+
+Compilation. If the CMS and the view are separate, then they won't need to recompile together all the time.
+also, the view can be developed with static content pretty easily, as simple templates, then later 'hydrated' when it comes to that point in the dev process
+Maybe also an advantage of having the 'layout' mindset for a solid session
+
+Also dart has types, js does not.
+
+>>>>build the thing that YOU want to use.
+
+*/
+
 // `main` binds elements and functions with event listeners
 function main() {
   // Toggle modal
