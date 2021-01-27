2368df78821aaa933f466c1511152c64c9553d71
jesserwight <jessewright01@gmail.com>
2021-01-27 15:21:25 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-# this is not how docker is supposed to be used?  :D (also, this wasy copy-paste)
-npm install &&
-npm run build:prod &&
 docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/myapp -w /usr/src/myapp rust:latest cargo build --release
